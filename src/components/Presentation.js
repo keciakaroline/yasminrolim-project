a3c3577f3da8f3b9e2e7f3fd88a771567e6aca58
Kécia Santos <keciakaroline@gmail.com>
2022-04-08 19:16:45 +0200

    trying to map images

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import "./styles/Presentation.css";
 
 export default function Presentation() {
-  function importAll(response) {
+  function importAll(require) {
     let images = {};
-    response.keys().map((item, index) => {
-      images[item.replace("./", "")] = response(item);
+    require.keys().forEach((item) => {
+      images[item.replace("./", "")] = require(item);
     });
     return images;
   }
+
   const images = importAll(
-    require.context("../images", false, /\.(png|jpe?g|svg)$/)
+    require.context("../faceImg", false, /\.(png|jpe?g|svg)$/)
   );
+
+  // const [model, setModel] = useState(false);
+  // const [tempimgsrc, setTempImgSrc] = useState("");
+
+  //const getImg = (images) => {
+  //  setTempImgSrc(images);
+  //  setModel(true);
+  //};
   return (
-    <div className="Presentation">
-      <img src={images["p1.jpg"]} alt="" />
-    </div>
+    <>
+      {/* <div className={model ? "model open" : "model"}>
+        <img src={tempimgsrc} alt="" />
+      </div> */}
+
+      {/* <div className="Presentation" onClick={getImg}>
+        <img src={images["p1.jpg"]} alt="" className="img-fluid img-project" />
+      </div> */}
+
+      {images.map((item, index) => {
+        return (
+          <img src={item["p22.jpg"]} alt="" className="img-fluid img-project" />
+        );
+      })}
+    </>
   );
 }
