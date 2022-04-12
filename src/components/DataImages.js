1929dd1276e4568f89793af3fb3c9fbf162e28c8
Kécia Santos <keciakaroline@gmail.com>
2022-04-12 22:31:38 +0200

    Added a database

---
+++ src/components/DataImages.js
@@ -0,0 +1,141 @@
+const dataImages = {
+  project_1: {
+    p1_1: require("../images/FaceImages/p1_1.jpg"),
+    p1_2: require("../images/p1/p1_2.jpg"),
+  },
+  project_2: {
+    p2_1: require("../images/FaceImages/p2_1.jpg"),
+    p2_2: require("../images/p2/p2_2.jpg"),
+  },
+  project_3: {
+    p3_1: require("../images/FaceImages/p3_1.jpg"),
+    p3_2: require("../images/p3/p3_2.jpg"),
+  },
+  project_4: {
+    p4_1: require("../images/FaceImages/p4_1.jpg"),
+    p4_2: require("../images/p4/p4_2.jpg"),
+  },
+  project_5: {
+    p5_1: require("../images/FaceImages/p5_1.jpg"),
+    p5_2: require("../images/p5/p5_2.jpg"),
+  },
+  project_6: {
+    p6_1: require("../images/FaceImages/p6_1.jpg"),
+    p6_2: require("../images/p6/p6_2.jpg"),
+    p6_3: require("../images/p6/p6_3.jpg"),
+    p6_4: require("../images/p6/p6_4.jpg"),
+  },
+  project_7: {
+    p7_1: require("../images/FaceImages/p7_1.jpg"),
+    p7_2: require("../images/p7/p7_2.jpg"),
+    p7_3: require("../images/p7/p7_3.jpg"),
+    p7_4: require("../images/p7/p7_4.jpg"),
+  },
+  project_8: {
+    p8_1: require("../images/FaceImages/p8_1.jpg"),
+    p8_2: require("../images/p8/p8_2.jpg"),
+  },
+  project_9: {
+    p9_1: require("../images/FaceImages/p9_1.jpg"),
+    p9_2: require("../images/p9/p9_2.jpg"),
+    p9_3: require("../images/p9/p9_3.jpg"),
+  },
+  project_10: {
+    p10_1: require("../images/FaceImages/p10_1.jpg"),
+    p10_2: require("../images/p10/p10_2.jpg"),
+    p10_3: require("../images/p10/p10_3.jpg"),
+  },
+  project_11: {
+    p11_1: require("../images/FaceImages/p11_1.jpg"),
+    p11_2: require("../images/p11/p11_2.jpg"),
+    p11_3: require("../images/p11/p11_3.jpg"),
+  },
+  project_12: {
+    p12_1: require("../images/FaceImages/p12_1.jpg"),
+    p12_2: require("../images/p12/p12_2.jpg"),
+    p12_3: require("../images/p12/p12_3.jpg"),
+    p12_4: require("../images/p12/p12_4.jpg"),
+  },
+  project_13: {
+    p13_1: require("../images/FaceImages/p13_1.jpg"),
+    p13_2: require("../images/p13/p13_2.jpg"),
+    p13_3: require("../images/p13/p13_3.jpg"),
+  },
+  project_14: {
+    p14_1: require("../images/FaceImages/p14_1.jpg"),
+    p14_2: require("../images/p14/p14_2.jpg"),
+    p14_3: require("../images/p14/p14_3.jpg"),
+    p14_4: require("../images/p14/p14_4.jpg"),
+  },
+  project_15: {
+    p15_1: require("../images/FaceImages/p15_1.jpg"),
+    p15_2: require("../images/p15/p15_2.jpg"),
+    p15_3: require("../images/p15/p15_3.jpg"),
+    p15_4: require("../images/p15/p15_4.jpg"),
+    p15_5: require("../images/p15/p15_5.jpg"),
+    p15_6: require("../images/p15/p15_6.jpg"),
+  },
+  project_16: {
+    p16_1: require("../images/FaceImages/p16_1.jpg"),
+    p16_2: require("../images/p16/p16_2.jpg"),
+    p16_3: require("../images/p16/p16_3.jpg"),
+    p16_4: require("../images/p16/p16_4.jpg"),
+  },
+  project_17: {
+    p17_1: require("../images/FaceImages/p17_1.jpg"),
+    p17_2: require("../images/p17/p17_2.jpg"),
+    p17_3: require("../images/p17/p17_3.jpg"),
+    p17_4: require("../images/p17/p17_4.jpg"),
+    p17_5: require("../images/p17/p17_5.jpg"),
+    p17_6: require("../images/p17/p17_6.jpg"),
+    p17_7: require("../images/p17/p17_7.jpg"),
+    p17_8: require("../images/p17/p17_8.jpg"),
+  },
+  project_18: {
+    p18_1: require("../images/FaceImages/p18_1.jpg"),
+    p18_2: require("../images/p18/p18_2.jpg"),
+    p18_3: require("../images/p18/p18_3.jpg"),
+  },
+  project_19: {
+    p19_1: require("../images/FaceImages/p19_1.jpg"),
+    p19_2: require("../images/p19/p19_2.jpg"),
+    p19_3: require("../images/p19/p19_3.jpg"),
+    p19_4: require("../images/p19/p19_4.jpg"),
+  },
+  project_20: {
+    p20_1: require("../images/FaceImages/p20_1.jpg"),
+    p20_2: require("../images/p20/p20_2.jpg"),
+    p20_3: require("../images/p20/p20_3.jpg"),
+  },
+  project_21: {
+    p21_1: require("../images/FaceImages/p21_1.jpg"),
+    p21_2: require("../images/p21/p21_2.jpg"),
+    p21_3: require("../images/p21/p21_3.jpg"),
+    p21_4: require("../images/p21/p21_4.jpg"),
+    p21_5: require("../images/p21/p21_5.jpg"),
+    p21_6: require("../images/p21/p21_6.jpg"),
+  },
+  project_22: {
+    p22_1: require("../images/FaceImages/p22_1.jpg"),
+    p22_2: require("../images/p22/p22_2.jpg"),
+    p22_3: require("../images/p22/p22_3.jpg"),
+  },
+  project_23: {
+    p23_1: require("../images/FaceImages/p23_1.jpg"),
+    p23_2: require("../images/p23/p23_2.jpg"),
+    p23_3: require("../images/p23/p23_3.jpg"),
+  },
+  project_24: {
+    p24_1: require("../images/FaceImages/p24_1.jpg"),
+    p24_2: require("../images/p24/p24_2.jpg"),
+    p24_3: require("../images/p24/p24_3.jpg"),
+    p24_4: require("../images/p24/p24_4.jpg"),
+  },
+  project_25: {
+    p25_1: require("../images/FaceImages/p25_1.jpg"),
+    p25_2: require("../images/p25/p25_2.jpg"),
+    p25_3: require("../images/p25/p25_3.jpg"),
+  },
+};
+
+export default dataImages;
