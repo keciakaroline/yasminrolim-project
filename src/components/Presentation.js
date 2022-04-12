a22ff05640f32c397dc4a317ee9df665fbf123fc
Kécia Santos <keciakaroline@gmail.com>
2022-04-12 22:32:48 +0200

    Link and added the project component to open model

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -4,11 +4,14 @@ import PhotoLibraryIcon from "@mui/icons-material/PhotoLibrary";
 import { NavLink } from "react-router-dom";
 import "./styles/Presentation.css";
 import Projects from "./Projects";
+// import dataImages from "./DataImages";
+// import "react-image-gallery/styles/css/image-gallery.css";
+// import ImageGallery from "react-image-gallery";
 
-function importAll(require) {
+function importAll(r) {
   let images = {};
-  require.keys().forEach((item) => {
-    images[item.replace("./", "")] = require(item);
+  r.keys().forEach((item) => {
+    images[item.replace("./", "")] = r(item);
   });
   return images;
 }
@@ -17,23 +20,14 @@ export default function Presentation() {
   const [model, setModel] = useState(false);
   const [tempImgSrc, setTempImgSrc] = useState("");
   const [images, setImages] = useState({});
-  const [newImages, setNewImages] = useState({});
-  const [miniImages, setMiniImages] = useState("");
 
   useEffect(() => {
     const tmpImages = importAll(
-      require.context("../faceImg", false, /\.(png|jpe?g|svg)$/)
+      require.context("../images/FaceImages", false, /\.(png|jpe?g|svg)$/)
     );
     //console.log(tmpImages);
     setImages(tmpImages);
   }, []);
-
-  // useEffect(() => {
-  //   const moreImages = importAll(
-  //     require.context("../projImg", false, /\.(png|jpe?g|svg)$/)
-  //   );
-  //   setNewImages(moreImages);
-  // }, []);
 
   const getImg = useCallback(
     (index) => {
@@ -44,40 +38,25 @@ export default function Presentation() {
   );
 
   return (
-    <>
-      <div className="Presentation">
-        {Object.keys(images).map((index) => (
-          <>
-            <img
-              src={images[index]}
-              alt=""
-              onClick={() => getImg(index)}
-              className="img-fluid img-project"
-            />
+    <div className="Presentation">
+      {Object.keys(images).map((index) => (
+        <img
+          src={images[index]}
+          alt=""
+          onClick={() => getImg(index)}
+          className="img-fluid"
+        />
+      ))}
 
-            <div className={model ? "model open" : "model"}>
-              <img src={tempImgSrc} alt="" className="big-image" />
-              <Projects />
-
-              <CloseIcon
-                className="close-icon"
-                onClick={() => setModel(false)}
-              />
-              {/* {Object.keys(newImages).map((index) => (
-                <img src={newImages[index]} />
-              ))} */}
-              <NavLink
-                exact
-                to={`/projects/${index}`}
-                className="btn btn-primary"
-              >
-                Teste 03
-                <PhotoLibraryIcon />
-              </NavLink>
-            </div>
-          </>
-        ))}
+      <div className={model ? "model open" : "model"}>
+        <img src={tempImgSrc} alt="" className="big-image" />
+        <Projects />;{/* <ImageGallery items={dataImages.project_2} /> */}
+        <CloseIcon className="close-icon" onClick={() => setModel(false)} />
+        {/* <NavLink exact to={`/projects/${index}`} className="btn btn-primary">
+            Teste 03
+            <PhotoLibraryIcon />
+          </NavLink> */}
       </div>
-    </>
+    </div>
   );
 }
