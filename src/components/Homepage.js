a8edaccb4239d77809adbdf9cd4622408d23c21e
Kécia Santos <keciakaroline@gmail.com>
2022-04-14 16:46:42 +0200

    Added style to homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,22 +1,17 @@
 import React from "react";
 import profile from "../images/yasmin-profile.png";
+import logo from "../images/simbol.png";
 import CodeBy from "./CodeBy";
 import Presentation from "./Presentation";
+import "./styles/Homepage.css";
 
 export default function Homepage() {
   return (
     <div className="Homepage">
-      <header className="container-fluid">
-        <div className="row">
-          <div className="col introdution">
-            <h1>Yasmin Rolim</h1>
-            <h3>Arquitetura & Interiores</h3>{" "}
-            {/*add letter-spacing on styles */}
-          </div>
-          <div className="col d-none d-lg-block">
-            <img src={profile} alt="Yasmin Rolim" className="img-fluid" />
-          </div>
-        </div>
+      <header>
+        <img src={logo} alt="" />
+        <h1>Yasmin Rolim</h1>
+        <h3>Arquitetura & Interiores</h3>{" "}
       </header>
       <Presentation />
       {/* Need to add a contact foot section: <ContactFooter />*/}
